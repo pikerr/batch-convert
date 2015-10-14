1c9461b7d046488410085d5c851f5b8c95382c65
Peter Bobov <peter.bobov@gmail.com>
2015-10-14 14:54:15 +0300

    Cleaned. Added readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+Description
+---
+Script to convert all wav files in specified directory to mp3 format.
+
+Requires avconv tool.
--- convert-wav-mp3.sh
+++ convert-wav-mp3.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 wav_dir=$1 
-out_dir="/var/www/pub/sound/"
 
-for f in `find "${wav_dir}" -mindepth 1 -maxdepth 1 -type f -name "*.wav" -print0 |
-  xargs -0 -I {} basename {} .wav` #| xargs -I {} avconv -i "{}.wav" "{}.mp3" && rm "{}.wav"
+for f in `
+  find "${wav_dir}" -mindepth 1 -maxdepth 1 -type f -name "*.wav" -print0 |
+  xargs -0 -I {} basename {} .wav`
 do
   avconv -i "$1/$f.wav" "$1/$f.mp3" && rm "$1/$f.wav"
 done
-
-#for i in $wav_dir/*
-#do
-#  echo "$i"
-#done
-
